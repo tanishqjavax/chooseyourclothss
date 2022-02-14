7e83f9536932bb5055139b1bece51151cad04ca4
Tanishak <tanishq.javax@gmail.com>
2022-02-14 16:23:57 +0530

    dd

---
+++ wastescrapper/src/main/java/com/wastescrapper/app/WastescrapperApplication.java
@@ -0,0 +1,13 @@
+package com.wastescrapper.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WastescrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WastescrapperApplication.class, args);
+	}
+
+}
