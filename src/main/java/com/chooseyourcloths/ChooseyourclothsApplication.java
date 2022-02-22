23d5f3eeb8f604f2cef5a7f4f7046c97a1af8a32
Tanishak <tanishq.javax@gmail.com>
2022-02-22 23:37:14 +0530

    finalprject

---
+++ src/main/java/com/chooseyourcloths/ChooseyourclothsApplication.java
@@ -0,0 +1,13 @@
+package com.chooseyourcloths;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChooseyourclothsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChooseyourclothsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chooseyourcloths/ChooseyourclothsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chooseyourcloths;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChooseyourclothsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
